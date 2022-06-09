9a63492db3e86511b9ed7a25941e494dc2c61654
SimoNapo02 <simonematteonapolitano@gmail.com>
2022-06-09 21:21:10 +0200

    search bar aggiunta

---
+++ src/components/blocks/BaseHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <SearchBar />
+  </header>
+</template>
+
+<script>
+import SearchBar from "../section/searchBar.vue";
+export default {
+  name: "BaseHeader",
+  components: { SearchBar },
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  height: 6.25rem;
+  background-color: black;
+  img {
+    width: 7.125rem;
+    margin-top: 0.3125rem;
+  }
+}
+</style>
---
+++ src/components/blocks/BaseMain.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <SectionFilm />
+  </main>
+</template>
+
+<script>
+import SectionFilm from "../section/sectionFilm.vue";
+export default {
+  name: "BaseMain",
+  components: { SectionFilm },
+};
+</script>
+
+<style></style>
